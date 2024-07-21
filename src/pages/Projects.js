7c07f66bbc9d22dc604aeec235a03e4ed0aed344
Bobby Davidson <bobbyjames839@gmail.com>
2024-07-21 09:10:58 +0100

    uodate

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -34,9 +34,9 @@ export const Projects = () => {
 
       <Project 
       title='Type 1 Diabetes Machine Learning Model' 
-      desc='Created a web app where I input data on every meal that I have eaten, this data is automatically sent to a google form. When I have enough data I will make a model that predicts how much insulin I should inject after a meal.' 
+      desc='Created a web app where I input data on every meal that I have eaten, this data is automatically sent to a google form. When I have enough data I will make a model that predicts how much insulin I should inject after a meal. Currently building V3.' 
       language='JavaScript, Google Forms' 
-      link = 'https://github.com/bobbyjames839/Diabetes-Data-Collector-V2'
+      link = 'https://github.com/bobbyjames839/Diabetes-Data-Collector-V3'
       website='https://diabetesv2.netlify.app'></Project>
 
       <Project 
